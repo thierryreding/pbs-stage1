4932b0760cae9c549d494bf591525c40952102fe
Thierry Reding <thierry.reding@gmail.com>
2013-09-17 21:45:25 +0200

    Mark non-hard-float ARM targets as soft-float
    arm-unknown-linux-gnueabi and arm-unknown-linux-uclibceabi targets are
    soft-float by convention.
    
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- targets/arm-unknown-linux-gnueabi.mk
+++ targets/arm-unknown-linux-gnueabi.mk
@@ -3,5 +3,6 @@ cpu = unknown
 os = linux
 libc = gnu
 abi = eabi
+fp = soft
 
 target := $(arch)-$(cpu)-$(os)-$(libc)$(abi)
--- targets/arm-unknown-linux-uclibceabi.mk
+++ targets/arm-unknown-linux-uclibceabi.mk
@@ -3,5 +3,6 @@ cpu = unknown
 os = linux
 libc = uclibc
 abi = eabi
+fp = soft
 
 target := $(arch)-$(cpu)-$(os)-$(libc)$(abi)
