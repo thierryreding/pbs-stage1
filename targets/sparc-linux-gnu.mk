c412ad7f068485ad27a821bd40162c44970bfef8
Thierry Reding <thierry.reding@gmail.com>
2015-05-07 12:31:00 +0200

    targets: Add sparc-unknown-linux-gnu target
    This is the proper GNU triplet for the SPARC GNU/Linux target, so use it
    instead of the old sparc-linux-gnu.
    
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- targets/sparc-linux-gnu.mk
+++
@@ -1,5 +0,0 @@
-arch = sparc
-os = linux
-libc = gnu
-
-target := $(arch)-$(os)-$(libc)
---
+++ targets/sparc-unknown-linux-gnu.mk
@@ -0,0 +1,6 @@
+arch = sparc
+vendor = unknown
+os = linux
+libc = gnu
+
+target := $(arch)-$(vendor)-$(os)-$(libc)
